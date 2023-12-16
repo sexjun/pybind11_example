a0410a09d0f9d2fc1c02c6f1752f233adcbe4a11
chendongsheng <sexluna@outlook.com>
2023-12-16 22:18:53 +0800

    add flatc demo

--- ir/src/main.cpp
+++ ir/src/main.cpp
@@ -1,25 +1,63 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
+#include <fstream>
+#include "flatbuffers/flatbuffers.h"
+#include "cds_generated.h"
 
 using namespace std;
 
+string filePath = "./results.bin";
+
+void serialization_data(string user_name, uint8_t user_age)
+{
+    flatbuffers::FlatBufferBuilder builder;
+    auto name = builder.CreateString(user_name);
+    dshengChen::Son son(0.1, 0.2, 0.3);
+    auto m = dshengChen::CreateMonster(builder, &son, name, user_age, dshengChen::Color_Green);
+    builder.Finish(m);
+    // 获取 FlatBuffer 数据指针和大小
+    const uint8_t *buffer = builder.GetBufferPointer();
+    size_t size = builder.GetSize();
+    std::ofstream outfile(filePath, std::ios::binary);
+    std::vector<char> data(size);
+    std::memcpy(data.data(), buffer, size);
+    outfile.write(data.data(), size);
+    outfile.close();
+}
+
+void deserialization_data()
+{
+    ifstream in_file(filePath, std::ios::binary);
+    if (in_file.is_open())
+    {
+        in_file.seekg(0, std::ios::end);
+        std::streamsize fileSize = in_file.tellg();
+        in_file.seekg(0, std::ios::beg);
+        // 读取文件内容到缓冲区
+        std::vector<char> buffer(fileSize);
+        in_file.read(buffer.data(), fileSize);
+        using namespace dshengChen;
+        auto parsed_person = GetMonster(buffer.data());
+        int age = parsed_person->age();
+        Color c = parsed_person->color();
+        cout << "\t age is: " << age;
+        cout << "\t name is: " << parsed_person->name()->c_str();
+        cout << "\t color is: " << (int)c << endl;
+    }
+    // 关闭文件
+    in_file.close();
+}
+
+bool test_flatbuffer()
+{
+    serialization_data("热爱学习", 12);
+    deserialization_data();
+    return true;
+}
+
 int main()
 {
-    cout << "thk" << endl;
-    std::string cmd = "flatc --version";
-    int status = system(cmd.c_str());
-    if (status)
-    {
-        cout << "error to find flatc";
-    }
-
-    std::string cpp_cmd = "flatc --cpp -o output ";
-    std::filesystem::path ir_path = std::filesystem::current_path().append("schema/example.fbs");
-    cpp_cmd += ir_path.string();
-    cout << "execute cmd: " << cpp_cmd;
-    // system(cpp_cmd.c_str());
-    cout << "generate on : output";
-
+    test_flatbuffer();
     return 0;
 }
--- script/gen_schma.sh
+++ script/gen_schma.sh
@@ -4,7 +4,7 @@ cpp_schema_gen="$(dirname $0)/../core/include/"
 python_schema_gen="$(dirname $0)/../python/"
 
 # gen cpp header
-flatc --cpp -o "${cpp_schema_gen}" /Users/chendongsheng/github/py11/ir/schema/example.fbs
+flatc --cpp -o "${cpp_schema_gen}" /Users/chendongsheng/github/py11/ir/schema/cds.fbs
 
 # gen python header
-flatc --python -o "${python_schema_gen}" /Users/chendongsheng/github/py11/ir/schema/example.fbs
+flatc --python -o "${python_schema_gen}" /Users/chendongsheng/github/py11/ir/schema/cds.fbs
