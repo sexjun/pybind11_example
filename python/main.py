a423cc93633b4ff3976b37b10dd313875804f74b
dsheng.chen <1196586501@qq.com>
2025-02-21 21:54:44 +0800

    feat: style changes
    1. code style
    2. update pybind11 version
    
    Signed-off-by: dsheng.chen <1196586501@qq.com>

--- python/main.py
+++ python/main.py
@@ -1,17 +1,16 @@
 import os
 import sys
 
-sys.path.append("/Users/chendongsheng/github/py11/build/core")
-sys.path.append("/Users/chendongsheng/github/py11/build/")
+from pathlib import Path
+
+buid_path = Path(__file__).parent.parent / "build/core"
+
+sys.path.append(str(buid_path))
+sys.path.append(str(buid_path.parent))
+
 
 import cmake_example
 
 cmake_example.sayHi()
-print(cmake_example.hi(4,9))
-print(cmake_example.add(1,2))
-
-
-
-# import example
-# p = example.Pet("Molly")
-# print(p.getName())
+print(cmake_example.hi(4, 9))
+print(cmake_example.add(1, 2))
--- script/gen_schma.sh
+++ script/gen_schma.sh
@@ -3,8 +3,12 @@
 cpp_schema_gen="$(dirname $0)/../core/include/"
 python_schema_gen="$(dirname $0)/../python/"
 
+script_path=$(dirname "$0")/../ir/schema/cds.fbs
+
+echo ${script_path}
+
 # gen cpp header
-flatc --cpp -o "${cpp_schema_gen}" /Users/chendongsheng/github/py11/ir/schema/cds.fbs
+flatc --cpp -o "${cpp_schema_gen}" ${script_path}
 
 # gen python header
-flatc --python -o "${python_schema_gen}" /Users/chendongsheng/github/py11/ir/schema/cds.fbs
+flatc --python -o "${python_schema_gen}" ${script_path}
